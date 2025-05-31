185c9f62287182ff3a1773fc0cc330900fb46976
Rameel Haroon <RameelHaroonOff@gmail.com>
2025-05-31 22:34:38 +0500

    Added fully functional UI for the game using JS

--- scripts.js
+++ scripts.js
@@ -1,39 +1,53 @@
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
+const resetbtn = document.querySelector("#reset-btn");
 
-function playGame(){
+rockBtn.addEventListener("click", () => {
+    const playerSelection = rockBtn.getAttribute('data-choice');
+    playRound(playerSelection, getComputerChoice());
+});
 
-    let humanSelection;
-    let computerSelection;
+paperBtn.addEventListener("click", () => {
+    const playerSelection = paperBtn.getAttribute('data-choice');
+    playRound(playerSelection, getComputerChoice());
+});
 
-    for(let i = 0; i < 5; i++){
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    console.log("Human Score:" + humanScore);
-    console.log("Computer Score:" + computerScore);
+scissorsBtn.addEventListener("click", () => {
+    const playerSelection = scissorsBtn.getAttribute('data-choice');
+    playRound(playerSelection, getComputerChoice());
+});
 
-    if(humanScore > computerScore){
-        console.log("Human WON!");
-    }else if(humanScore < computerScore){
-        console.log("Computer WON!");
-    }else{
-        console.log("Game Drawn");
-    }
-}
+
+// Reset game
+resetbtn.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    document.querySelector(".human-score").textContent = humanScore;
+    document.querySelector(".computer-score").textContent = computerScore;
+    document.querySelector("#result-bar").textContent = "NEW GAME STARTED!";
+    resetbtn.style.display = "none";
+
+    // Unhide game buttons
+    rockBtn.style.display = "inline-block";
+    paperBtn.style.display = "inline-block";
+    scissorsBtn.style.display = "inline-block";
+});
 
 function playRound(humanSelection, computerSelection){
     humanSelection = humanSelection.toLowerCase();
+    const resultBar = document.querySelector("#result-bar");
     let hasPlayerWon = false;
 
     if(humanSelection === computerSelection){
-        console.log("Draw");
+        resultBar.textContent = "DRAW";
         return;
     }
 
+    // Check if human won the round or not
     if((humanSelection === "rock" && computerSelection === "scissors") 
         || (humanSelection === "paper" && computerSelection === "rock") 
         || (humanSelection === "scissors" && computerSelection === "paper")){
@@ -42,11 +56,35 @@ function playRound(humanSelection, computerSelection){
 
     if(hasPlayerWon){
         humanScore += 10;
-        console.log("Player has won the round");
+        document.querySelector(".human-score").textContent = humanScore;
+        resultBar.textContent = "PLAYER WON THE ROUND";
     }else{
         computerScore += 10;
-        console.log("Computer has won the round");
+        document.querySelector(".computer-score").textContent = computerScore;
+        resultBar.textContent = "COMPUTER WON THE ROUND";
     }
+
+    let hideBtns = 0;
+
+    // Check for game end
+    if(humanScore === 50){
+        resultBar.textContent = "🎉 CONGRATULATIONS! YOU WON!";
+        document.querySelector("#reset-btn").style.display = "inline-block";
+        hideBtns = 1;
+    }
+
+    if(computerScore === 50){
+        resultBar.textContent = "😢 YOU LOST! TRY AGAIN.";
+        document.querySelector("#reset-btn").style.display = "inline-block";
+        hideBtns = 1;
+    }
+
+    if(hideBtns){
+        rockBtn.style.display = "none";
+        paperBtn.style.display = "none";
+        scissorsBtn.style.display = "none";
+    }
+    
 }
 
 function getComputerChoice(){
@@ -65,10 +103,5 @@ function getComputerChoice(){
             break;
     }
     return compChoice;
-}
-
-function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice");
-    return humanChoice;
 }
 
